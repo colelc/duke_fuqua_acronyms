a16f6435aec35c0b889667d96e7221c54d95c1d8
Linda Cole <colelc@gmail.com>
2024-02-16 11:41:46 -0500

    Commit

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -1,11 +1,9 @@
-//import { HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AcronymsAdminViewComponent } from './acronyms-admin-view/acronyms-admin-view.component';
 import { AcronymsFooterComponent } from './acronyms-footer/acronyms-footer.component';
 import { AcronymsUserViewComponent } from './acronyms-user-view/acronyms-user-view.component';
-//import { AuthInterceptor } from './interceptor/auth-interceptor';
 import { AcronymsService } from './service/acronyms.service';
 import { ConfigService } from './service/config.service';
 import { HttpService } from './service/http.service';
@@ -20,17 +18,15 @@ import { environment } from '../environments/environment';
   imports: [ 
     AcronymsFooterComponent, 
     AcronymsAdminViewComponent, AcronymsUserViewComponent
-    //, HttpClientModule
     , FormsModule
     ,RouterOutlet
     , RouterLink
   ],
-  providers: [AcronymsService, UserService, HttpService, ConfigService, /*AuthInterceptor,*/ Router
-   // , {provide: HTTP_INTERCEPTORS, useClass:AuthInterceptor, multi: true}
+  providers: [AcronymsService, UserService, HttpService, ConfigService, Router
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  schemas: [/*CUSTOM_ELEMENTS_SCHEMA*/] // need this for PrimeNg to work
+  schemas: [/*CUSTOM_ELEMENTS_SCHEMA*/] // would need this for PrimeNg to work
 })
 
 export class AppComponent implements OnInit {
@@ -57,11 +53,12 @@ export class AppComponent implements OnInit {
 
   home() {
     //const authData = document.querySelector('fw-auth').getData();
-    const authData = document.querySelector('fw-auth');
-    console.log("authData", authData);
+    //const authData = document.querySelector('fw-auth');
+    //console.log("authData", authData);
     this.administrator = this.userService.isUserAdmin();
 
     if (this.administrator === true) {
+      console.log("ADMIN");
       this.router.navigateByUrl("/acronyms/admin");
     } else {
       console.log("USER");
