20b57b79af475ae9cf06c01451ac01470ca75193
Linda Cole <colelc@gmail.com>
2024-02-22 12:39:57 -0500

    commit

--- angular-frontend/src/environments/environment.development.ts
+++ angular-frontend/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiBase: 'https://localhost.fuqua.duke.edu:8444',
-    //apiBase: 'https://go-dev.fuqua.duke.edu',
+    //apiBase: 'https://localhost.fuqua.duke.edu:8444',
+    apiBase: 'https://rivers.fuqua.duke.edu:8444',
     where: 'development'
 };
--- angular-frontend/src/environments/environment.ts
+++ angular-frontend/src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: true,
     where: 'production',
-    apiBase: 'https://localhost.fuqua.duke.edu:8444',
+    //apiBase: 'https://localhost.fuqua.duke.edu:8444',
+    apiBase: 'https://rivers.fuqua.duke.edu:8444',
     //apiBase: 'https://go.fuqua.duke.edu',
 };
